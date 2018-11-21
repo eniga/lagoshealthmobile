9eb4a31d5df7e98bbd377c9efc823254f5461acd
Eniga Ahiante <enigaahiante@live.com>
2018-11-21 21:45:51 +0100

    Added forgot password page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { AuthGuardService } from './services/auth-guard.service';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './public/login/login.module#LoginPageModule' },
-  { path: 'dashboard', loadChildren: './members/dashboard/dashboard.module#DashboardPageModule' }
+  { path: 'forgot', loadChildren: './public/forgot/forgot.module#ForgotPageModule' },
+  {
+    path: 'members',
+    canActivate: [AuthGuardService],
+    loadChildren: './members/member-routing.module#MemberRoutingModule'
+  },
+  { path: 'forgot', loadChildren: './public/forgot/forgot.module#ForgotPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
